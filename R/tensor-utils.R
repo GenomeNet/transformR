b3ad73c77f3d30ce741e99311f00cf31b9d13ec8
Philipp Münch <Philipp.Muench@helmholtz-hzi.de>
2020-11-17 18:45:11 +0100

    replace shape_list2

--- R/tensor-utils.R
+++ R/tensor-utils.R
@@ -1,54 +1,19 @@
 
 #' Grabs list of tensor dims statically, where possible.
-shape_list <- 
-  function(x) {
-    
-    x <- tf$convert_to_tensor(x)
-    
-    dims <- x$get_shape()$dims
-    if (is.null(dims)) return(tf$shape(x))
-    
-    sess <- tf$compat$v1$keras$backend$get_session()
-    
-    shape <- tf$shape(x)$eval(session = sess)
-    
-    ret <- vector('list', length(dims))
-    
-    purrr::map2(dims, shape, function(x, y) {
-      dim <- x
-      
-      if (is.null(dim)) 
-        dim <- y
+shape_list <- function(x) {
+    shape <- x$shape 
+    l <- list()
+    for (i in 1:length(shape)) {
+      l[[i]] <- shape[[i]]
+    }
+    return(l)    
+}
 
-      dim
-      
-    })
-  }
-
-#' Can we cheat and call value on Dimension 
-#' class object without getting into trouble?
-shape_list2 <- 
-  function(x) {
-    
-    x <- tf$convert_to_tensor(x)
-    
-    dims <- x$get_shape()$dims    
-    if (is.null(dims)) return(tf$shape(x))
-    
-    dims <- purrr::map(dims, ~.$value)
-    
-    sess <- tf$compat$v1$keras$backend$get_session()
-    shape <- tf$shape(x)$eval(session = sess)
-    
-    ret <- vector('list', length(dims))
-    
-    purrr::map2(dims, shape, function(x, y) {
-      dim <- x
-      
-      if (is.null(dim)) 
-        dim <- y
-      
-      dim
-      
-    })
-  }
+shape_list2 <- function(x) {
+    shape <- x$shape 
+    l <- list()
+    for (i in 1:length(shape)) {
+      l[[i]] <- shape[[i]]
+    }
+    return(l)    
+}
